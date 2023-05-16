e9a74816635048abfd3c30eab33329e6b33b53ec
Ruyangchen <3226717170@qq.com>
2023-05-16 15:15:24 +0800

    Site updated: 2023-05-16 15:15:23

--- js/main.js
+++ js/main.js
@@ -1,226 +1,144 @@
-function loadScript(isAsync, url, cb, isMoudule) {
-    let script = document.createElement('script');
-    script.src = url;
-    if (cb) script.onload = cb;
-    if (isMoudule) script.type = 'module';
-    if (!isAsync) { isAsync = true };
-    script.async = isAsync;
-    document.body.appendChild(script);
-}
-function prompt_core(msg, fd_t, click) {
-    let div = document.createElement("div");
-    div.innerHTML = `${msg}`;
-    div.id = "prompt_1";
-    div.style = "position: fixed; top: 20px;text-align: center; background: #fff; color: #000; left: 50vw;padding: 10px 25px; border-radius: 20px;transition: .35s;z-index: 99;font-size: 0.9rem;font-weight: 300;font-family: 'Avenir', Helvetica, Arial, sans-serif;transform: translate(-50%,0)!important";
-    div.style.opacity = 0;
-    // 点击消失
-    if (click) {
-        div.onclick = () => {
-            document.getElementById("prompt_1").style.opacity = 0;
-            setTimeout(() => {
-                document.getElementById("prompt_1").remove();
-            }, 1000);
-        }
+require([], function (){
+
+    var isMobileInit = false;
+    var loadMobile = function(){
+        require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
+            mobile.init();
+            isMobileInit = true;
+        });
     }
-    document.body.appendChild(div);
-    // 渐入效果
-    setTimeout(() => {
-        document.getElementById("prompt_1").style.opacity = 1;
-    }, 100);
-    setTimeout(() => {
-        try {
-            document.getElementById("prompt_1").style.opacity = 0;
-            setTimeout(() => {
-                document.getElementById("prompt_1").remove();
-            }, 1000);
-        } catch (e) {}
-    }, fd_t);
-}
-
-/**
- * loadScriptFile
- * option: object
- * url: script 地址
- * cb: script onload 事件
- * module: 是否为模块
- * loadType: async/defer
- * TIP: 这是对 loadScript() 的 Breaking Changes.
- */
-function loadScriptFile(option) {
-    let script = document.createElement('script');
-    script.src = option.url;
-    if (option.cb) script.onload = option.cb;
-    if (option.module) script.type = 'module';
-    script[option.loadType] = true;
-    document.body.appendChild(script);
-}
-/**
- * 选择器
- * query(".class") => <div class="class"></div>
- * query("#id") => <div id="id"></div>
- * query("label") => <label></label>
- */
-function query(selector) {
-    return Array.from(document.querySelectorAll(selector));
-}
-/**
- * addClass
- * e: CSS 选择器
- * c: 添加的 Class，可为 string / object
- */
-function addClass(e, c) {
-    // 对 element 进行遍历
-    for (let i = 0; i < query(e).length; i++) {
-        if (typeof c == "object") {
-            // 如果给了一个 object: ["class1", "class2"]
-            for (let o = 0; o < c.length; o++) {
-                query(e)[i].classList.add(c[o]);
-            }
-        } else {
-            // 其他的格式: "class"
-            query(e)[i].classList.add(c);
-        }
+    var isPCInit = false;
+    var loadPC = function(){
+        require([yiliaConfig.rootUrl + 'js/pc.js'], function(pc){
+            pc.init();
+            isPCInit = true;
+        });
     }
-}
-/**
- * removeClass
- * e: CSS 选择器
- * c: 删除的 Class，可为 string / object
- */
-function removeClass(e, c) {
-    // 对 element 进行遍历
-    for (let i = 0; i < query(e).length; i++) {
-        if (typeof c == "object") {
-            // 如果给了一个 object: ["class1", "class2"]
-            for (let o = 0; o < c.length; o++) {
-                query(e)[i].classList.remove(c[o]);
-            }
-        } else {
-            // 其他的格式: "class"
-            query(e)[i].classList.remove(c);
-        }
+
+    var browser={
+        versions:function(){
+        var u = window.navigator.userAgent;
+        return {
+            trident: u.indexOf('Trident') > -1, //IE内核
+            presto: u.indexOf('Presto') > -1, //opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, //是否为iPhone或者安卓QQ浏览器
+            iPad: u.indexOf('iPad') > -1, //是否为iPad
+            webApp: u.indexOf('Safari') == -1 ,//是否为web应用程序，没有头部与底部
+            weixin: u.indexOf('MicroMessenger') == -1 //是否为微信浏览器
+            };
+        }()
     }
-}
-/**
- * inArray
- * 检索 String 是否存在于 Array 中
- * stringToSearch: String
- * arrayToSearch: Array
- */
-function inArray(stringToSearch, arrayToSearch) {
-    for (s = 0; s < arrayToSearch.length; s++) {
-        thisEntry = arrayToSearch[s].toString();
-        if (thisEntry == stringToSearch) {
-            return true;
-        }
-    }
-    return false;
-}
-/**
- * toggleClass
- * e: CSS 选择器
- * c: 切换的 Class，可为 string / object
- */
-function toggleClass(e, c) {
-    // 对 element 进行遍历
-    for (let i = 0; i < query(e).length; i++) {
-        if (typeof c == "object") {
-            // 如果给了一个 object: ["class1", "class2"]
-            for (let o = 0; o < c.length; o++) {
-                inArray(c[o], query(e)[i].classList) ? removeClass(e, c[o]) : addClass(e, c[o]);
-            }
-        } else {
-            inArray(c, query(e)[i].classList) ? removeClass(e, c) : addClass(e, c);
-        }
-    }
-}
 
-/**
- * Darkmode
- */
-(() => {
-    const rootElement = document.documentElement;
-    const darkModeStorageKey = 'user-color-scheme';
-    const darkModeMediaQueryKey = '--color-mode';
-    const rootElementDarkModeAttributeName = 'data-theme';
-
-    const setLS = (k, v) => {
-        try {
-            localStorage.setItem(k, v);
-        } catch (e) { }
-    };
-
-    const removeLS = (k) => {
-        try {
-            localStorage.removeItem(k);
-        } catch (e) { }
-    };
-
-    const getLS = (k) => {
-        try {
-            return localStorage.getItem(k);
-        } catch (e) {
-            return null;
-        }
-    };
-
-    const getModeFromCSSMediaQuery = () => {
-        const res = getComputedStyle(rootElement).getPropertyValue(darkModeMediaQueryKey);
-        if (res.length) return res.replace(/["'\s]/g, '');
-        return res === 'dark' ? 'dark' : 'light';
-    };
-
-    const resetRootDarkModeAttributeAndLS = () => {
-        rootElement.removeAttribute(rootElementDarkModeAttributeName);
-        removeLS(darkModeStorageKey);
-    };
-
-    const validColorModeKeys = {
-        dark: true,
-        light: true,
-    };
-
-    const applyCustomDarkModeSettings = (mode) => {
-        const currentSetting = mode || getLS(darkModeStorageKey);
-
-        if (currentSetting === getModeFromCSSMediaQuery()) {
-            resetRootDarkModeAttributeAndLS();
-        } else if (validColorModeKeys[currentSetting]) {
-            rootElement.setAttribute(rootElementDarkModeAttributeName, currentSetting);
-
-        } else {
-            resetRootDarkModeAttributeAndLS();
-        }
-    };
-
-    const invertDarkModeObj = {
-        dark: 'light',
-        light: 'dark',
-    };
-
-    const toggleCustomDarkMode = () => {
-        var currentSetting = getLS(darkModeStorageKey);
-
-        if (validColorModeKeys[currentSetting]) {
-            currentSetting = invertDarkModeObj[currentSetting];
-        } else if (currentSetting === null) {
-            currentSetting = invertDarkModeObj[getModeFromCSSMediaQuery()];
-        } else {
+    $(window).bind("resize", function(){
+        if(isMobileInit && isPCInit){
+            $(window).unbind("resize");
             return;
         }
-        setLS(darkModeStorageKey, currentSetting);
-
-        return currentSetting;
-    };
-
-    applyCustomDarkModeSettings();
-
-    document.onreadystatechange = function () {
-        if (document.readyState == "complete") {
-            const darkModeToggleBottonElement = document.getElementById('btn-toggle-dark');
-            darkModeToggleBottonElement.addEventListener('click', () => {
-                applyCustomDarkModeSettings(toggleCustomDarkMode());
-            });
+        var w = $(window).width();
+        if(w >= 700){
+            loadPC();
+        }else{
+            loadMobile();
         }
+    });
+
+    if(browser.versions.mobile === true || $(window).width() < 700){
+        loadMobile();
+    }else{
+        loadPC();
     }
-})();
+
+    //是否使用fancybox
+    if(yiliaConfig.fancybox === true){
+        require([yiliaConfig.rootUrl + 'fancybox/jquery.fancybox.js'], function(pc){
+            var isFancy = $(".isFancy");
+            if(isFancy.length != 0){
+                var imgArr = $(".article-inner img");
+                for(var i=0,len=imgArr.length;i<len;i++){
+                    var src = imgArr.eq(i).attr("src");
+                    var title = imgArr.eq(i).attr("alt");
+                    imgArr.eq(i).replaceWith("<a href='"+src+"' title='"+title+"' rel='fancy-group' class='fancy-ctn fancybox'><img src='"+src+"' title='"+title+"'></a>");
+                }
+                $(".article-inner .fancy-ctn").fancybox();
+            }
+        });
+
+    }
+    //是否开启动画
+    if(yiliaConfig.animate === true){
+
+        require([yiliaConfig.rootUrl + 'js/jquery.lazyload.js'], function(){
+            //avatar
+            $(".js-avatar").attr("src", $(".js-avatar").attr("lazy-src"));
+            $(".js-avatar")[0].onload = function(){
+                $(".js-avatar").addClass("show");
+            }
+        });
+
+      if(yiliaConfig.isHome === true) {
+        // 滚动条监听使用scrollreveal.js
+        // https://github.com/jlmakes/scrollreveal.js
+        // 使用cdn[//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js]
+        require([
+          '//cdn.bootcss.com/scrollReveal.js/3.0.5/scrollreveal.js'
+        ], function (ScrollReveal) {
+          // 更多animation:
+          // http://daneden.github.io/animate.css/
+          var animationNames = [
+            "pulse", "fadeIn","fadeInRight", "flipInX", "lightSpeedIn","rotateInUpLeft", "slideInUp","zoomIn",
+            ],
+            len = animationNames.length,
+            randomAnimationName = animationNames[Math.ceil(Math.random() * len) - 1];
+
+          // ie9 不支持css3 keyframe动画, safari不支持requestAnimationFrame, 不使用随机动画，切回原来的动画
+          if (!window.requestAnimationFrame) {
+              $('.body-wrap > article').css({opacity: 1});
+
+              if (navigator.userAgent.match(/Safari/i)) {
+                  function showArticle(){
+                      $(".article").each(function(){
+                          if( $(this).offset().top <= $(window).scrollTop()+$(window).height() && !($(this).hasClass('show')) ) {
+                              $(this).removeClass("hidden").addClass("show");
+                              $(this).addClass("is-hiddened");
+                          }else{
+                              if(!$(this).hasClass("is-hiddened")){
+                                  $(this).addClass("hidden");
+                              }
+                          }
+                      });
+                  }
+                  $(window).on('scroll', function(){
+                      showArticle();
+                  });
+                  showArticle();
+              }
+              return;
+          }
+          // document.body有些浏览器不支持监听scroll，所以使用默认的document.documentElement
+          ScrollReveal({
+            duration: 0,
+            afterReveal: function (domEl) {
+              // safari不支持requestAnimationFrame不支持document.documentElement的onscroll所以这里不会执行
+              // 初始状态设为opacity: 0, 动画效果更平滑一些(由于脚本加载是异步，页面元素渲染后在执行动画，感觉像是延时)
+              $(domEl).addClass('animated ' + randomAnimationName).css({opacity: 1});
+            }
+          }).reveal('.body-wrap > article');
+
+        });
+      } else {
+        $('.body-wrap > article').css({opacity: 1});
+      }
+
+    }
+
+    //是否新窗口打开链接
+    if(yiliaConfig.open_in_new == true){
+        $(".article a[href]").attr("target", "_blank")
+    }
+    $(".archive-article-title").attr("target", "_blank");
+});
